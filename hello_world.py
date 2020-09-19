be1dd001a1e5e6abe5e0319bfa2d84e3bcfd1a2f
xKatooo <eusebioc913@gmail.com>
2020-09-19 05:16:33 -0500

    hola mundo

---
+++ hello_world.py
@@ -0,0 +1,6 @@
+from collections import OrderedDict
+exec("".join(map(chr,[int("".join(str(OrderedDict([(':)', 0), (':D', 1), (':P', 2), (':S', 3), (':(', 4), ('=)', 5), ('=/', 6), (':/', 7), (':{', 8), (';)', 9)])[i]) for i in x.split())) for x in
+":D :D :P  :D :D :(  :D :) =)  :D :D :)  :D :D =/  :( :)  :S :(  :D :) \
+:(  :D :) :D  :D :) :{  :D :) :{  :D :D :D  :S :P  :D :D ;)  :D :D :D \
+ :D :D :(  :D :) :{  :D :) :)  :S :(  :( :D  :D :)"
+.split("  ")])))
